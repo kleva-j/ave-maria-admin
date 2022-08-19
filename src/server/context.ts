4c27c2abf619104e833a9acb37857c1591c89ada
kleva-j <kasmickleva@gmail.com>
2022-08-19 09:17:50 +0100

    feat(trpc-server): create trpc server - setup trpc server with websocket connection. - connect user session from next-auth to trpc server. - setup req routing to posts. - setup subscriptions to post. - setup prisma client database. - create schema and migrations for prisma database

---
+++ src/server/context.ts
@@ -0,0 +1,30 @@
+import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+
+import ws from 'ws';
+
+import { NodeHTTPCreateContextFnOptions } from '@trpc/server/adapters/node-http';
+import { getSession } from 'next-auth/react';
+import { prisma } from './db/prismaClient';
+import { IncomingMessage } from 'http';
+
+/**
+ * Creates context for an incoming request
+ * @link https://trpc.io/docs/context
+ */
+export const createContext = async ({
+  req,
+  res,
+}:
+  | trpcNext.CreateNextContextOptions
+  | NodeHTTPCreateContextFnOptions<IncomingMessage, ws>) => {
+  const session = await getSession({ req });
+  return {
+    req,
+    res,
+    prisma,
+    session,
+  };
+};
+
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
---
+++ src/server/db/prismaClient.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-var */
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
