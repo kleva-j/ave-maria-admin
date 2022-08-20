fc2731420cc26b07275f4a240247ca0ac22afdcb
kleva-j <kasmickleva@gmail.com>
2022-08-20 01:35:19 +0100

    create helper function for handling the signup/signin logic

---
+++ src/helpers/index.tsx
@@ -0,0 +1,30 @@
+export const getBaseUrl = () => {
+  if (typeof window !== 'undefined') {
+    return '';
+  }
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
+export const requirements = [
+  { re: /[0-9]/, label: 'Includes number' },
+  { re: /[a-z]/, label: 'Includes lowercase letter' },
+  { re: /[A-Z]/, label: 'Includes uppercase letter' },
+  { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: 'Includes special symbol' },
+];
+
+export function getStrength(password: string) {
+  let multiplier = password.length > 5 ? 0 : 1;
+
+  requirements.forEach((requirement) => {
+    if (!requirement.re.test(password)) {
+      multiplier += 1;
+    }
+  });
+
+  return Math.max(100 - (100 / (requirements.length + 1)) * multiplier, 10);
+}
+
+export const isEmpty = (obj: Record<string, unknown>) =>
+  Object.keys(obj).length === 0;
---
+++ src/lib/auth.ts
@@ -0,0 +1,9 @@
+import { hash, compare } from 'bcryptjs';
+
+export async function hashPassword(password: string) {
+  return hash(password, 12);
+}
+
+export async function verifyPassword(password: string, hashedPassword: string) {
+  return compare(password, hashedPassword);
+}
