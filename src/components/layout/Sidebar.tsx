4d5f0a983e3ac1e4648fadcf3798b82ac0336901
kleva-j <kasmickleva@gmail.com>
2022-09-05 00:04:36 +0100

    display event timeline component in the sidebar section

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,10 +1,14 @@
-import { Aside, Text, MediaQuery } from '@mantine/core';
+import { Aside, Title, MediaQuery } from '@mantine/core';
+import { TimeLine } from 'components/admin/Timeline';
 
 export const PageSidebar = () => {
   return (
     <MediaQuery smallerThan="md" styles={{ display: 'none' }}>
       <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-        <Text>Application sidebar</Text>
+        <Title order={4} my="md">
+          Event Timeline
+        </Title>
+        <TimeLine />
       </Aside>
     </MediaQuery>
   );
