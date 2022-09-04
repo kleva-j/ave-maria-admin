9750da3dc7a54392824e58075758eea8b41352d3
kleva-j <kasmickleva@gmail.com>
2022-09-05 00:06:49 +0100

    add usercapsule and logout button components to the navbar

--- src/components/admin/Navbar.tsx
+++ src/components/admin/Navbar.tsx
@@ -1,28 +1,50 @@
+import { NavbarButton } from 'components/molecules/NavbarButton';
+import { UserCapsule } from 'components/molecules/UserCapsule';
 import { ButtonLink } from 'components/molecules/ButtonLink';
-import { Navbar, Stack } from '@mantine/core';
+import { MediaQuery, Navbar, Stack } from '@mantine/core';
+import { IoMdExit } from 'react-icons/io';
+import { signOut } from 'next-auth/react';
 import { AdminRoutes } from 'lib/routes';
+import { useRouter } from 'next/router';
 
 export const AdminNavbar = (props: { opened: boolean }) => {
   const { opened } = props;
+  const { asPath } = useRouter();
+  const path = asPath.split('/').slice(-1)[0];
 
   return (
     <Navbar
-      width={{ sm: 160, lg: 240 }}
+      width={{ sm: 220, lg: 300 }}
       hiddenBreakpoint="sm"
       hidden={!opened}
-      p="md"
+      p="sm"
     >
       <Navbar.Section grow mt="xs">
         <Stack>
-          {Object.values(AdminRoutes).map((route) => (
-            <ButtonLink
-              key={route.href}
-              href={route.href}
-              Icon={route.icon}
-              text={route.pathname}
-            />
-          ))}
+          {Object.values(AdminRoutes).map(
+            ({ Component, icon: Icon, ...route }) => (
+              <Component
+                key={route.href}
+                href={route.href}
+                icon={<Icon />}
+                label={route.pathname}
+                isActive={path === route.matcher}
+              />
+            ),
+          )}
         </Stack>
+      </Navbar.Section>
+      <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+        <Navbar.Section>
+          <NavbarButton
+            Icon={IoMdExit}
+            onClick={() => signOut()}
+            text="Log out"
+          ></NavbarButton>
+        </Navbar.Section>
+      </MediaQuery>
+      <Navbar.Section>
+        <UserCapsule />
       </Navbar.Section>
     </Navbar>
   );
