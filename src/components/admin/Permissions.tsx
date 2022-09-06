8cde21d0088842b34a201066e53b4efc4fcb2231
kleva-j <kasmickleva@gmail.com>
2022-09-06 23:53:02 +0100

    add permissions tab to admin settings

---
+++ src/components/admin/Permissions.tsx
@@ -0,0 +1,13 @@
+import { Table, Tabs } from '@mantine/core';
+
+export const Permissions = () => {
+  return (
+    <Tabs.Panel value="permissions" pt="xs">
+      Permissions
+      <Table>
+        <thead></thead>
+        <tbody></tbody>
+      </Table>
+    </Tabs.Panel>
+  );
+};
