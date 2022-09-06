09f7eca227e7519ce0669fc8376fc3ecfe0f1f0f
kleva-j <kasmickleva@gmail.com>
2022-09-06 23:53:57 +0100

    add integrations tab to admin settings

---
+++ src/components/admin/Integrations.tsx
@@ -0,0 +1,9 @@
+import { Tabs } from '@mantine/core';
+
+export const Integrations = () => {
+  return (
+    <Tabs.Panel value="integrations" pt="xs">
+      Integrations
+    </Tabs.Panel>
+  );
+};
